5c1eb40440c8e6c27ca628e8933e3da471cbc005
Gagandeep-2003 <125819661+Gagandeep-2003@users.noreply.github.com>
2024-05-23 17:23:46 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { HiMenuAlt4, HiX } from "react-icons/hi";
 import { useLenis } from "@studio-freight/react-lenis";
 import { gsap } from 'gsap';
 import "./Navbar.css";
+import gagancv from "../../constants/cv"
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
@@ -10,19 +11,17 @@ const Navbar = () => {
   const navLinksRef = useRef([]);
 
   useEffect(() => {
-  const t1 = gsap.timeline();
-  
-   // Fade in with bounce animation
-   t1.from(navLinksRef.current, {
-    duration: 1,
-    opacity: 0,
-    y: -50,
-    stagger: 0.2,
-    ease: "bounce.out",
-  })
+    const t1 = gsap.timeline();
 
- 
-}, []);
+    // Fade in with bounce animation
+    t1.from(navLinksRef.current, {
+      duration: 1,
+      opacity: 0,
+      y: -50,
+      stagger: 0.2,
+      ease: "bounce.out",
+    });
+  }, []);
 
   return (
     <nav className="app__navbar">
@@ -31,7 +30,7 @@ const Navbar = () => {
           <li
             className="app__flex p-text"
             key={`link-${item}`}
-            ref={el => navLinksRef.current[index] = el}
+            ref={(el) => (navLinksRef.current[index] = el)}
           >
             <div></div>
             <a onClick={() => lenis.scrollTo(`#${item}`)} href={`#${item}`}>
@@ -40,13 +39,19 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div className="app__navbar-cv">
+      
+     <a href={gagancv} download="Gagan_cv" className="cv-link">
+  Download CV
+</a>
 
+
+      
+      </div>
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
-
         <div className={`app__navbar-list ${toggle ? "active" : ""}`}>
           <HiX onClick={() => setToggle(false)} />
-
           <ul>
             {["home", "about", "work", "skills", "contact"].map((item, index) => (
               <li key={item}>
