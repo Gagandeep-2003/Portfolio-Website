c06afd2feba508a2fa0a63e9143384f3909999e0
Gagandeep Singh <125819661+Gagandeep-2003@users.noreply.github.com>
2024-10-03 13:53:13 +0530

    added loading animation✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import "./App.css";
-
-import { Header, About, Work, Skills, Footer } from "./container";
-import { Navbar } from "./components";
-import { ReactLenis } from "@studio-freight/react-lenis";
+import React from 'react';
+import { ReactLenis } from '@studio-freight/react-lenis';
+import { Header, About, Work, Skills, Footer } from './container';
+import { Navbar } from './components';
+import LoadingPage from './container/LoadingPage/loadingPage';
+import MouseFollowerEffect from './components/MouseFollowerEffect/MouseFollowerEffect'; 
+import './App.css';
 
 const App = () => {
   return (
-    // ReactLenis is used for smooth scrolling
-    <ReactLenis root options={{ lerp: 0.1, duration: 1, smoothTouch: true }}> 
+    <ReactLenis root options={{ lerp: 0.1, duration: 1, smoothTouch: true }}>
       <div className="app">
+        <MouseFollowerEffect /> 
         <Navbar />
         <Header />
         <About />
         <Work />
         <Skills />
         <Footer />
+        <LoadingPage />
       </div>
     </ReactLenis>
   );
